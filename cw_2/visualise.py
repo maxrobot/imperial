131ef5931f92cd5e7fd51dc825fc11046e0b2f81
fgrabner <fmg215@aews254-017.ae.ic.ac.uk>
2016-12-22 17:13:11 +0000

    Switch from cw 1 to cw 2

---
+++ cw_2/visualise.py
@@ -0,0 +1,29 @@
+#----------------------------------------------------------#
+#	Friedrich M. Grabner - 01220997
+#----------------------------------------------------------#
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+
+for x in xrange(1,30):
+	# Open the first file
+	crs = open('./data/output000' + str(x),'r')
+	D = np.loadtxt(crs)
+
+	# Create Subplot
+	ax = plt.subplot(111)
+	ax.set_title('Vorticity')
+	# quadmesh = ax.pcolormesh(X,Y,data)
+	# colormap will be used for the contour lines
+	im = plt.imshow(D,interpolation='bilinear', origin='lower',extent=(0,1,0,1))
+	im.set_clim(vmin=-.25, vmax=.25)
+
+	plt.xlabel('x axis')
+	plt.ylabel('y axis')
+	if x==1:
+		CBI = plt.colorbar(im,orientation='horizontal', shrink=0.8)
+
+	# We can still add a colorbar for the image, too.
+	# Save figure...
+	plt.savefig('./images/temp000' + str(x))
